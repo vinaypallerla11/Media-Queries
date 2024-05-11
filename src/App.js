0eb59adb477257b1ef25066c2164d3db46680e67
vinaypallerla11 <vinaypallerla96@gmail.com>
2024-05-11 19:00:11 +0530

    media queries

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className='head'>hello vinay</h1>
+      <p className='para'>Pushpa’s story is an extraordinary one. This 40-year-old woman from Chennai, Tamil Nadu, has led a life of transformation. She was a child labourer, she became an empowered domestic worker and is now a leader. Her life is testimony to the powerful role of social movements and the impact that women’s leadership can produce! The youngest of the five siblings, Pushpa was the only daughter to her parents. Having lost her father at a young age, as a young girl Pushpa not only supported her mom in the household chores but was also assisted her at her workplace. A typical day for Pushpa started with accompanying her mother every morning to distribute milk packets to individual households, followed by proceeding to work as a domestic worker in a four-member household. During the one and a half hours she spent there, the young Pushpa had to wash clothes, clean the house and the courtyard, and support in the kitchen. Then she would rush back home to attend to the needs of her brothers at home and later prepare for school. Often reaching school late, Pushpa would regularly be punished by the teachers! When the school day ended and she would go to work again — the evening shift. The demands on her time Impacted her school work, and she couldn’t clear her Board exams!</p>
+      <div className='image-container'>
+        <img className='image' src="https://www.freewalldownload.com/katrina-kaif/katrina-kaif-simple-looks-famous-beauty.jpg" alt="img2"/>
+        <img className='image' src="https://www.freewalldownload.com/yami-gautam/beautiful-smile-of-yami-gautam.jpg" alt="images"/>
+      </div>
     </div>
   );
 }
